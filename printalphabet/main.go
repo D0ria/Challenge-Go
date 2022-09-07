0746567146a6de867f7152a1285ed442662682b8
D0ria <doriaremadna@gmail.com>
2022-09-07 14:33:50 +0200

    ...

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	"github.com/01-edu/z01"
+    "github.com/01-edu/z01"
 )
 
 func main() {
-	for i := 97; i < 122; i++ {
-		z01.PrintRune(rune(i))
-}
+
+    z01.PrintRune('a')
+
+
+
+    for compteur := 97; compteur < 122; compteur ++ {
+
+        z01.PrintRune(rune(compteur))
+
+
+    }
+
+
 }
