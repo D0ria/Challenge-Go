4e6368357db97aff3134998d41dc7dc64cf1c089
D0ria <doriaremadna@gmail.com>
2022-09-12 15:31:57 +0200

    test 1 2 1 2

--- isprintable.go
+++ isprintable.go
@@ -3,10 +3,10 @@ package piscine
 func IsPrintable(s string) bool {
 	booleen := true
 	for _, val := range s {
-		if val == '0' || val == '\n' {
-			booleen = false
-		} else {
+		if val >= 32 && val <= 127 {
 			booleen = true
+		} else {
+			booleen = false
 		}
 	}
 	return booleen
