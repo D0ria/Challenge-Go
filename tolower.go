929100e1eb6d6e69824b9ecf5a29b3f4a20fcec1
D0ria <doriaremadna@gmail.com>
2022-09-12 16:46:10 +0200

    YESSSS

--- tolower.go
+++ tolower.go
@@ -3,11 +3,11 @@ package piscine
 func ToLower(s string) string {
 	var a string
 	for i := 0; i < len(s); i++ {
-		if s[i] <= 65 && s[i] >= 90 {
+		if s[i] >= 65 && s[i] <= 90 {
 			a += string((rune(s[i]) + 32))
 		} else {
 			a += string((rune(s[i])))
 		}
-		return a
 	}
+	return a
 }
