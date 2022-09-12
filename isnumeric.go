88295f3b2d5f02200437b84d0f662342d38a2e18
D0ria <doriaremadna@gmail.com>
2022-09-12 12:04:34 +0200

    test 1 2 1 2

--- isnumeric.go
+++ isnumeric.go
@@ -2,9 +2,9 @@ package piscine
 
 func IsNumeric(s string) bool {
 	bool := true
-	for _, val := range s {
-		if val >= '0' && val <= '9' {
-			bool = true
+	val1 := []rune(s)
+	for i := range val1 {
+		if val1[i] >= 48 && val1[i] <= 57 {
 		} else {
 			bool = false
 		}
