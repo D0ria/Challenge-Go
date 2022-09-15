7ad7148f79cd78ea8059a713d6587d539d286ce9
D0ria <doriaremadna@gmail.com>
2022-09-15 11:29:28 +0200

    iughbnkiouyghj

---
+++ index.go
@@ -0,0 +1,15 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	for i := 0; i < len(s)-len(toFind); i++ {
+		for j := range toFind {
+			if s[i+j] != toFind[j] {
+				break
+			}
+			if j == len(toFind)-1 {
+				return i
+			}
+		}
+	}
+	return -1
+}
