989f0c7d5bc84617fdb63c41be7f9eb061d40257
D0ria <doriaremadna@gmail.com>
2022-09-15 14:18:12 +0200

    gss

--- index.go
+++ index.go
@@ -1,6 +1,9 @@
 package piscine
 
 func Index(s string, toFind string) int {
+	if len(toFind) == 0 {
+		return 0
+	}
 	for i := 0; i < len(s)-len(toFind); i++ {
 		for j := range toFind {
 			if s[i+j] != toFind[j] {
@@ -8,9 +11,6 @@ func Index(s string, toFind string) int {
 			}
 			if j == len(toFind)-1 {
 				return i
-			}
-			if len(toFind) == 0 {
-				return 0
 			}
 		}
 	}
