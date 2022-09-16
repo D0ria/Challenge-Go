13277432dceeebaa399ebd688ccefecfec868a30
D0ria <doriaremadna@gmail.com>
2022-09-16 11:23:40 +0200

    rezsdf

--- capitalize.go
+++ capitalize.go
@@ -5,7 +5,7 @@ func Capitalize(s string) string {
 	for i := 0; i < len(s); i++ {
 		if i != 0 && (s[i-1] < 48 || s[i-1] > 57) && (s[i-1] < 65 || s[i-1] > 90) && (s[i-1] < 97 || s[i-1] > 122) && (s[i] >= 97 && s[i] <= 122) {
 			vegeta += string(rune(s[i] - 32))
-		} else if i != 0 && ((s[i-1] >= 97 && s[i-1] <= 122) || (s[i-1] >= 65 && s[i-1] <= 90)) && (s[i] >= 65 && s[i] <= 90) && (s[i-1] >= 48 && s[i-1] <= 57) {
+		} else if i != 0 && ((s[i-1] >= 97 && s[i-1] <= 122) || (s[i-1] >= 65 && s[i-1] <= 90)) && (s[i] >= 65 && s[i] <= 90) && (s[i-1] >= 48 && s[i-1] <= 57) && (s[i+1] >= 48 && s[i+1] <= 57) {
 			vegeta += string((rune(s[i] + 32)))
 		} else if i == 0 && (s[i] >= 97 && s[i] <= 122) {
 			vegeta += string(rune(s[i] - 32))
