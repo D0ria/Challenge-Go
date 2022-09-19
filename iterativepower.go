5c0ce50a55d214a9f64a23418f66539462133429
D0ria <doriaremadna@gmail.com>
2022-09-19 11:08:26 +0200

    oiuyfgcvdbn

---
+++ iterativepower.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	puissance := 1
+	if nb == 0 {
+		return 0
+	}
+	if power == 0 {
+		return 1
+	}
+	for i := 0; i < power; i++ {
+		puissance *= nb
+	}
+	return puissance
+}
