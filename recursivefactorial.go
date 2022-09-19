e6c28187034a81b27b92ff4b917c95c979b37e71
D0ria <doriaremadna@gmail.com>
2022-09-19 10:39:16 +0200

    oiuyfgcvdbn

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,13 +1,14 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb <= 19 {
+	if nb <= 25 {
 		if nb == 0 {
 			return 1
 		}
 		if nb < 0 {
 			return 0
 		}
+		return nb * RecursiveFactorial(nb-1)
 	}
-	return nb * RecursiveFactorial(nb-1)
+	return 0
 }
