9dbcee804897135c95921942bbbf3dbdc32978aa
D0ria <doriaremadna@gmail.com>
2022-09-19 11:27:00 +0200

    oiuyfgcvdbn

--- iterativepower.go
+++ iterativepower.go
@@ -6,7 +6,10 @@ func IterativePower(nb int, power int) int {
 		return 0
 	}
 	if power < 0 {
-		return 1
+		return 0
+	}
+	if (nb < 0 && power > 0) || (nb > 0 && power < 0) {
+		return 0
 	}
 	for i := 0; i < power; i++ {
 		puissance *= nb
