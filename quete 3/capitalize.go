1a4b1bd2d1124f34f9cece3c75aa5e802c98229e
D0ria <doriaremadna@gmail.com>
2022-09-19 09:23:35 +0200

    oiuyfgcvdbn

---
+++ quete 3/capitalize.go
@@ -0,0 +1,17 @@
+package piscine
+
+func Capitalize(s string) string {
+	var vegeta string
+	for i := 0; i < len(s); i++ {
+		if i != 0 && (s[i-1] < 48 || s[i-1] > 57) && (s[i-1] < 65 || s[i-1] > 90) && (s[i-1] < 97 || s[i-1] > 122) && (s[i] >= 97 && s[i] <= 122) {
+			vegeta += string(rune(s[i] - 32))
+		} else if i != 0 && ((s[i-1] >= 97 && s[i-1] <= 122) || (s[i-1] >= 65 && s[i-1] <= 90)) && (s[i] >= 65 && s[i] <= 90) {
+			vegeta += string((rune(s[i] + 32)))
+		} else if i == 0 && (s[i] >= 97 && s[i] <= 122) {
+			vegeta += string(rune(s[i] - 32))
+		} else {
+			vegeta += string((rune(s[i])))
+		}
+	}
+	return vegeta
+}
