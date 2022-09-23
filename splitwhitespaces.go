ed143416354bfe742e7fd757138e5438cacb41bf
D0ria <doriaremadna@gmail.com>
2022-09-23 12:05:43 +0200

    iuytre

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -4,12 +4,13 @@ func SplitWhiteSpaces(s string) []string {
 	var s1 []string
 	var mot string
 	for j, i := range s {
-		mot += string(i)
 		if string(i) == " " {
 			s1 = append(s1, mot)
 			mot = ""
 		} else if j == len(s)-1 {
 			s1 = append(s1, mot)
+		} else {
+			mot += string(i)
 		}
 	}
 	return s1
