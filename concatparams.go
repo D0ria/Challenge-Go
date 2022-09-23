25cd1ba7f461f65ac2e6df17846570a1491b3ff2
D0ria <doriaremadna@gmail.com>
2022-09-23 11:18:15 +0200

    te

--- concatparams.go
+++ concatparams.go
@@ -2,8 +2,13 @@ package piscine
 
 func ConcatParams(args []string) string {
 	var phrase string
-	for _, i := range args {
-		phrase += i + "\n"
+	for a, i := range args {
+		phrase += i
+		if a == len(args)-1 {
+			continue
+		} else {
+			phrase += "\n"
+		}
 	}
 	return phrase
 }
