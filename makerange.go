47b85b9c8eb36a5afcd65c5799b573f7cfa7ecb6
D0ria <doriaremadna@gmail.com>
2022-09-23 10:31:22 +0200

    rez

---
+++ makerange.go
@@ -0,0 +1,16 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	var s1 []int
+	cpt := 0
+	if min >= max {
+		return nil
+	} else {
+		s1 = make([]int, min)
+		for i := min; i < max; i++ {
+			s1[cpt] = i
+			cpt += 1
+		}
+	}
+	return s1
+}
