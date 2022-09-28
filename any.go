c9de63e3b8ad795dfd53194e542d095bb55980f4
D0ria <doriaremadna@gmail.com>
2022-09-28 14:31:58 +0200

    any

--- any.go
+++ any.go
@@ -3,7 +3,9 @@ package piscine
 func Any(f func(string) bool, a []string) bool {
 	var booleen bool
 	for i := 0; i < len(a); i++ {
-		f(a[i])
+		if f(a[i]) {
+			booleen = true
+		}
 	}
 	return booleen
 }
