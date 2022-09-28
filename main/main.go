39808fd993e48648158e2763a4ce935f3ff43a7f
D0ria <doriaremadna@gmail.com>
2022-09-28 10:17:22 +0200

    ...

--- main/main.go
+++ main/main.go
@@ -1,22 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"piscine"
-
-	"github.com/01-edu/z01"
 )
 
 func main() {
 	a := []int{1, 2, 3, 4, 5, 6}
-	piscine.ForEach(PrintNbr, a)
-}
-
-func PrintNbr(n int) {
-	var tabint []int
-	for i := n; i > 0; i /= 10 {
-		tabint = append(tabint, i%10)
-	}
-	for i := len(tabint) - 1; i >= 0; i-- {
-		z01.PrintRune(rune(tabint[i] + 48))
-	}
+	result := piscine.Map(piscine.IsPrime, a)
+	fmt.Println(result)
 }
