c940c77168c5367644a0b5b7361c21b2b8fb1ddc
D0ria <doriaremadna@gmail.com>
2022-09-29 09:56:04 +0200

    facultatif

---
+++ countif.go
@@ -0,0 +1,7 @@
+package piscine
+
+func CountIf(f func(string) bool, tab []string) int {
+	var nb int
+
+	return nb
+}
--- main/main.go
+++ main/main.go
@@ -1,22 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"piscine"
-
-	"github.com/01-edu/z01"
 )
 
 func main() {
-	a := []int{1, 2, 3, 4, 5, 6}
-	piscine.ForEach(PrintNbr, a)
-}
-
-func PrintNbr(n int) {
-	var tabint []int
-	for i := n; i > 0; i /= 10 {
-		tabint = append(tabint, i%10)
-	}
-	for i := len(tabint) - 1; i >= 0; i-- {
-		z01.PrintRune(rune(tabint[i] + 48))
-	}
+	fmt.Println(piscine.Sqrt(4))
+	fmt.Println(piscine.Sqrt(3))
 }
