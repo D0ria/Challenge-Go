868d27e4f56a08b53dc7782a7e8ab31303cfbb1e
D0ria <doriaremadna@gmail.com>
2022-09-29 10:56:43 +0200

    trertrer

--- countif.go
+++ countif.go
@@ -2,7 +2,7 @@ package piscine
 
 func CountIf(f func(string) bool, tab []string) int {
 	var nb int
-	for i := 0; i < len(tab); i++ {
+	for i := 0; i <= len(tab); i++ {
 		if f(tab[i]) {
 			nb += i
 		}
