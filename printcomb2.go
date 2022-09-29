ffee47cdf1568578102df3297b6a7320c618f80f
D0ria <doriaremadna@gmail.com>
2022-09-29 16:42:59 +0200

    1rst

--- printcomb2.go
+++ printcomb2.go
@@ -6,29 +6,8 @@ func PrintComb2() {
 	for a := 0; a <= 9; a++ {
 		for b := 0; b <= 9; b++ {
 			for c := 0; c <= 9; c++ {
-				for d := c + 1; d <= 9; d++ {
-					if a < b || a == 9 && b == 9 {
-						z01.PrintRune(rune(a + 48))
-						z01.PrintRune(rune(b + 48))
-						z01.PrintRune(' ')
-						z01.PrintRune(rune(c + 48))
-						z01.PrintRune(rune(d + 48))
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
-					}
-				}
-			}
-		}
-	}
-	PrintComb2reverse()
-}
-
-func PrintComb2reverse() {
-	for a := 9; a >= 0; a-- {
-		for b := 9; b >= 0; b-- {
-			for c := 0; c <= 9; c++ {
 				for d := 0; d <= 9; d++ {
-					if a < b || a == 9 && b == 9 {
+					if a != 9 || b != 9 {
 						z01.PrintRune(rune(a + 48))
 						z01.PrintRune(rune(b + 48))
 						z01.PrintRune(' ')
